49fddfc5c0f26af32caa2b6b28ef3c176e7b0cce
ppillip <ppillip@gmail.com>
2020-04-08 17:54:34 +0900

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,31 @@
+//<-- 1.10
+
+import './lib/helper.js';
+import '../lib/collections.js';
+import '../lib/methods.js';
+
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './main.html';
+import './account.js';
+
+import "./createRoom.html";
+import "./createRoom.js";
+
+import "./roomHeader.html";
+import "./roomHeader.js";
+
+import "./roomList.html";
+import "./roomList.js";
+
+import "./messageList.html";
+import "./messageList.js";
+
+import "./messageInput.html";
+import "./messageInput.js";
+
+import "./message.html";
+import "./message.js";
+
+//-->
---
+++ server/main.js
@@ -0,0 +1,14 @@
+//<-- 1.10
+import { Meteor } from 'meteor/meteor';
+
+import '../lib/collections.js';
+import '../lib/methods.js';
+
+import './fixture.js';
+import './allow.js';
+import './publish.js';
+
+Meteor.startup(() => {
+
+});
+//-->
